11a01cf7e6ecde0e06f8d3b4755842cae3d4da79
Gabriel Brits <gabriel.brits@icloud.com>
2024-04-22 11:25:03 +1000

    added the XSS vulnerability, and parsed user inputs to the login form

--- site/server.js
+++ site/server.js
@@ -3,12 +3,26 @@ const path = require('node:path');
 
 const app = express();
 
+app.use(express.urlencoded({extended: false}));
+
 app.get('/admin', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/admin.html'));
 });
 
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/login.html'));
+});
+
+app.post('/login', (req, res) => {
+    const username = req.body.username; 
+    const password = req.body.password;
+    // This will get the username and password (Hayley needs to figure out how to then retrieve these values from the database)
+    res.send(`Username: ${username} Password: ${password}`);
+});
+
+app.get('/search', (req, res) => {
+    const searchQuery = req.query.q;
+    res.send('Search results for: ' + searchQuery);
 });
 
 app.get('/', (req, res) => {
