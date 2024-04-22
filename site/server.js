fe754f6cafb645f98f222008350421c58bf1adf1
Gabriel Brits <gabriel.brits@icloud.com>
2024-04-22 11:48:25 +1000

    added a vulnerability where anyone can act as a user and view peoples credentials through localhost:3000/user/userId

--- site/server.js
+++ site/server.js
@@ -5,6 +5,18 @@ const app = express();
 
 app.use(express.urlencoded({extended: false}));
 
+app.get('/user/:userId', async (req, res) => {
+    // Hayley, get the username and password from the userId
+    // to do this user something like: const username = sql.query("select username where id = req.params.userId");
+    const username = null;
+    const password = null;
+    if (username) {
+        res.send(`Username: ${username}<br>Password: ${password}`);
+    } else {
+        res.send('User not found');
+    }
+});
+
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/login.html'));
 });
@@ -12,8 +24,15 @@ app.get('/login', (req, res) => {
 app.post('/login', (req, res) => {
     const username = req.body.username; 
     const password = req.body.password;
-    // This will get the username and password (Hayley needs to figure out how to then retrieve these values from the database)
-    res.send(`Username: ${username} Password: ${password}`);
+    // This will get the username and password (Hayley needs to figure out how to then check these values from the database)
+    // You will need to make find if the username and password match and then return the ID here.
+    // once u got the id, reroute the user to the user profile page
+    const userId = null;
+    if (userId) {
+        res.redirect(`/user/${userId}`);
+    } else {
+        res.send('Invalid username or password');
+    }
 });
 
 app.get('/search', (req, res) => {
